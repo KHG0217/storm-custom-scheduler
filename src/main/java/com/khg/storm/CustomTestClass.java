f3f4e0cdffe2ff1315ee21df7aab5e4a21bb41ed
khg <hyukgyu3818@naver.com>
2025-07-28 11:37:09 +0900

    ST-1 시나리오를 위한 동일 매체 중복 배치 방지 로직 구현 (https://www.notion.so/ST-1-23a5af7f550c806a94eaf7ed2e35a95b?source=copy_link)

--- src/main/java/com/khg/storm/CustomTestClass.java
+++ src/main/java/com/khg/storm/CustomTestClass.java
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import java.lang.reflect.Array;
 import java.util.*;
 import java.util.stream.Collectors;
+import org.apache.storm.Config;
 
 public class CustomTestClass implements IScheduler {
     private static final Logger LOG = LoggerFactory.getLogger(CustomTestClass.class);
@@ -62,70 +63,94 @@ public class CustomTestClass implements IScheduler {
      */
     @Override
     public void schedule(Topologies topologies, Cluster cluster) {
-        // 1. Supervisor별로 어떤 그룹을 실행 중인지 기록하는 맵
-        Map<String, String> supervisorGroupMap = new HashMap<>();
 
-        // 2. 현재 할당된 모든 토폴로지 조사 → 그룹 키워드가 있는 경우 Supervisor 기록
-        // key = Topology ID, value = ScedulerAssignment(어떤 슬롯에서 실행 중인지)
-        Map<String, SchedulerAssignment> assignments = cluster.getAssignments();
+        for (TopologyDetails topology  : cluster.needsSchedulingTopologies()) {
+            String topologyName = topology.getName();
+            String group = extractGroup(topologyName);
 
-        for(Map.Entry<String, SchedulerAssignment> entry : assignments.entrySet()) {
-            String topologyId = entry.getKey();
-            SchedulerAssignment assignment = entry.getValue();
+            if (group == null) {
+                LOG.info("Group name couldn't be determined for topology: {}, skipping.", topologyName);
+                continue;
+            }
 
-            TopologyDetails td = topologies.getById(topologyId); // TopologyDetails 객체 (이름, 상태 ,자원 등 정보 포함된 객체)
-            if (td == null) continue;
+            // Supervisor 별 실행 중인 그룹 맵핑
+            Map<String, Set<String>> supervisorToGroups = getSupervisorToGroups(cluster, topologies);
 
-            String name = td.getName().toLowerCase();
-            String group = extractGroup(name);
+            // 해당 그룹이 아직 실행되지 않은 Supervisor만 필터
 
-            /**
-             * 그룹이 있는 경우 (group != null):
-             *  이 토폴로지가 할당된 슬롯(slot)의 Supervisor ID(nodeId)를 뽑아 supervisorGroupMap에 기록
-             *  즉, 이 Supervisor는 이미 다른 키워드가 포함된 토폴로지ID로 실행중임을 등록
-             */
-            for(WorkerSlot slot : assignment.getSlots()) {
-                if(group != null) {
-                    supervisorGroupMap.put(slot.getNodeId(), group);
+            // 현재 클러스터의 모든 Supervisor 객체들을 가져옴
+            // -> Map<String, SueprvisorDetails> 에서 value만 가져옴
+            List<SupervisorDetails> candidateSupervisors = cluster.getSupervisors().values().stream()
+                    .filter(s -> {
+                        Set<String> groups =  supervisorToGroups.get(s.getId()); // 현재 Supervisor가 실행중인 group 목록 가져오기
+                        if(groups == null) { // supervisorToGroups에 해당 supervisor가 없다면 아직 아무 group도 실행 안한 supervisor
+                            groups = new HashSet<>(); // 빈 Set으로 처리 (null 방지)
+                        }
+                        return !groups.contains(group); // 현재 스케줄링할 topology에 group이 이미 실행중인지 확인, 없으면 후보군으로 인정
+                    })
+                    .collect(Collectors.toList()); // 조건을 만족하는 Supervisoir들만 리스트로 수집
+
+            if(candidateSupervisors.isEmpty()) { // 해당 그룸을 이미 모든 Supervisor가 실행 중 -> 실행 안함
+                LOG.info("No available supervisor for group: {}, topologt: {}",group,topologyName);
+                continue;
+            }
+
+            // 가능한 Superviosr 중에서 free slot이 있는 Superviosr만 추출
+            List<WorkerSlot> availableSlots = new ArrayList<>();
+            for(SupervisorDetails supervisor: candidateSupervisors) {
+                Set<Integer> usedPorts = cluster.getUsedPorts(supervisor);
+                for(Integer port : supervisor.getAllPorts()) {
+                    if(!usedPorts.contains(port)) {
+                        availableSlots.add(new WorkerSlot(supervisor.getId(), port));
+                    }
                 }
             }
-        }
+            if(availableSlots.isEmpty()) {
+                LOG.info("No free worker slots for group: {}, topology: {}", group, topologyName);
+                continue;
+            }
 
-        // 3. 실행 대기 중인 토폴로지들 중 그룹이 있는 경우, 중복 Supervisor 제한
-        for(TopologyDetails td : topologies.getTopologies()) {
-            if("ACTIVE".equals(cluster.getStatus(td.getId()))) continue;
-
-            String name = td.getName().toLowerCase();
-            String group = extractGroup(name);
-            if(group == null) continue; // 그룹에 없는 토폴로지는 제약 없이 실행
-
+            // 할당할 executor
             /**
-             * 이미 같은 group을 실행하고 있는 Supervisor ID 목록을 모음
-             * 이 Supervisor들은 이번에 새로 배치될 토폴로지에서는 배제 대상이 됨
+             * cluster.getNeedsSchedulingComponentToExecutors(topology)
+             * Cluster 객체에서 topology에 대해 아직 실행되지 않은 componet -> executor 목록을 리턴
+             * 반환형 : Map<String, Collection<ExecutorDetails>>
+             *     - key: component 이름 (예: spout1, bolt1 등)
+             *     - value: 그 component에 연결된 executor들 (하나의 component는 여러 executor을 가질 수 있음)
+             *
+             * flatMap(Collection::stream)
+             * 중첩된 컬렉션을 하나로 풀어주는 역활, List<List<X>> -> List<X>와 같음
+             *
+             * 예)
+             * getNeedsSchedulingComponentToExecutors(topology) 아래의 예시라고 가정
+             * {
+             *   "spout1": [Executor(1,1), Executor(2,2)],
+             *   "bolt1":  [Executor(3,3)]
+             * }
+             *
+             * .values()는
+             * [
+             *   [Executor(1,1), Executor(2,2)],
+             *   [Executor(3,3)]
+             * ]
+             *
+             * -> .flatMap(Collection::stream)으로 펴면
+             * [Executor(1,1), Executor(2,2), Executor(3,3)]
+             *
+             * 즉 해당 로직은 스케줄링이 필요한 모든 executor 목록을 하나의 List로 만드는 로직
              */
-            Set<String> forbiddenSupervisors = supervisorGroupMap.entrySet().stream()
-                    .filter(e -> e.getValue().equals(group))
-                    .map(Map.Entry::getKey)
-                    .collect(Collectors.toSet());
+            List<ExecutorDetails> executors = cluster.getNeedsSchedulingComponentToExecutors(topology).values().stream()
+                    .flatMap(Collection::stream)
+                    .collect(Collectors.toList()); // executor들을 List로 수집
 
-            /**
-             * [정책 필터 적용]
-             * 모든 Supervisor에 대해 반복하며 이미 forbiddenSupervisors 에 포함된 Supervisor라면,
-             * 해당 Supervisor에 있는 할당 가능한 슬롯들을 사용하지 못하도록 해제 (freeSlots)
-             * 이로 인해 Strom의 기본 스케줄러는 그 Supervisor를 배치 후보에서 제외함
-             */
-            for(SupervisorDetails supervisor : cluster.getSupervisors().values()) { // 전체 supervisor의 정보 추출
-                if(forbiddenSupervisors.contains(supervisor.getId())) { // superviosr의 고유 ID 포함유무, 즉 같은 그룹에 포함된 슈퍼바이저인지 확인
-                    LOG.info("제한됨: {} 그룹으로 인해 Supervisor {} 제외됨", group, supervisor.getHost());
-                    /**
-                     * cluster.freeSlots(...) 해당 슬롯들을 Storm에서 할당하지 않도록 지정
-                     * cluster.getAssignableSlots(supervisor) 이 Supervisor에서 사용할 수 있는 슬롯을 반환, 즉 사용하고있지 않는 대기중인 슬롯들
-                     */
-                    cluster.freeSlots(cluster.getAssignableSlots(supervisor));
-                }
+            // 사용 가능한 가장 첫 번째 slot에 해당 execytor들을 전부 할당
+            try {
+                cluster.assign(availableSlots.get(0), topology.getId(), executors);
+                LOG.info("Assigned topology: {}, to slot: {}", topologyName, availableSlots.get(0));
+            } catch (Exception e) {
+                LOG.error("Failed to assign topology: {} to slot: {}", topologyName, availableSlots.get(0), e);
             }
         }
-        delegate.schedule(topologies, cluster); // 실행: 위임
     }
 
     @Override
@@ -144,4 +169,31 @@ public class CustomTestClass implements IScheduler {
                 .findFirst()
                 .orElse(null);
     }
+
+    /**
+     *
+     * @param cluster
+     * @param topologies
+     * @return
+     */
+    private Map<String, Set<String>> getSupervisorToGroups (Cluster cluster, Topologies topologies) {
+       Map<String, Set<String>> supervisorToGroups = new HashMap<>();
+
+       for (TopologyDetails topologyDetails : topologies.getTopologies()) {
+           SchedulerAssignment assignment = cluster.getAssignmentById(topologyDetails.getId());
+           if(assignment == null) {
+               LOG.info("[SKIP] Topology {} is not yet scheduled, skipping for supervisor-group mapping.", topologyDetails.getName());
+               continue;
+           }
+
+           String group = extractGroup(topologyDetails.getName());
+
+           for (WorkerSlot slot : assignment.getExecutorToSlot().values()) {
+               String supervisorId = slot.getNodeId();
+               supervisorToGroups.computeIfAbsent(supervisorId, k -> new HashSet<>()).add(group);
+           }
+       }
+       return supervisorToGroups;
+    }
+
 }
